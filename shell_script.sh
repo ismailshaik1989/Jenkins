8a18e0ab19d7d44c141423c3942460f92501e7b9
ismailshaik1989 <ismailshaik1989@gmail.com>
2019-08-05 14:24:30 -0400

    Update shell_script.sh

--- shell_script.sh
+++ shell_script.sh
@@ -3,5 +3,4 @@ echo "This script is pulled from Github"
 echo "This way we are configuring automated builds using Git"
 NUMBER=$(($NUMONE + $NUMTWO))
 echo "$NUMBER"
-echo "Adding of two number executed successfully"
 echo "After adding Githook to Jenkins config"
