da780ba5fe37421ab6532b387fa3f8f657729cc5
ismailshaik1989 <ismailshaik1989@gmail.com>
2019-08-05 14:35:35 -0400

    Update shell_script.sh

--- shell_script.sh
+++ shell_script.sh
@@ -4,4 +4,4 @@ echo "This way we are configuring automated builds using Git"
 NUMBER=$(($NUMONE + $NUMTWO))
 echo "$NUMBER"
 echo "After adding Githook to Jenkins config"
-echo "Hello Ismail from Git"
+echo "Hello Ismail from Gitzz"
